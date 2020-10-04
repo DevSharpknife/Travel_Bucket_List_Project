93dee107bfabb0841ecfa0efadec8598bba141bb
David Murphy <daveru@Davids-MacBook-Pro.local>
2020-10-04 18:16:39 +0100

    Added delete and delete all functions to country repo

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -24,3 +24,12 @@ def select(id):
     if result is not None:
         country = Country(result['name'], result['id'])
     return country
+
+def delete_all():
+    sql = "DELETE FROM countries"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM countries WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
